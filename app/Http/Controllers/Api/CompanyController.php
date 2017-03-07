b7eef3d92b22e94033194685e1355c9e8f3d1847
libo <libo@oppo.com>
2017-03-07 15:59:18 +0800

    商品

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Goods;
+use Illuminate\Http\Request;
+
+class GoodsController extends BaseApiController {
+
+    public function index() {
+        $data = Goods::OrderBy('sequence' , 'desc')->paginate();
+        return $this->apiReturn(true,'ok',$data);
+    }
+
+    public function show($id){
+        $data = Goods::findOrfail($id);
+        return $this->apiReturn(true,'ok',$data);
+    }
+
+    public function store(Request $request) {
+        $this->validate($request ,Goods::$rules);
+        $data = $request->all();
+        $data['sequence'] = Goods::max('sequence') + 1;
+        if ( Goods::create($data) ) {
+            return $this->apiReturn(true , '添加成功');
+        }
+    }
+
+    public function update(Request $request , $id) {
+        $goods = Goods::find($id);
+        if ( !$goods ) {
+            return $this->apiReturn(false , '更新失败');
+        }
+        if ( $goods->update($request->all()) ) {
+            return $this->apiReturn(true , '更新id为' . $id . '的数据成功');
+        }
+        return $this->apiReturn(false , '更新失败');
+    }
+
+    public function destroy($id) {
+        $goods = Goods::find($id);
+        if ( $goods ) {
+            $goods->delete();
+            return $this->apiReturn(true , '删除成功');
+        }
+        return $this->apiReturn(false , '删除失败');
+    }
+
+    public function sort(Request $request) {
+        $goodss = $request->all();
+        $index = count($goodss);
+        foreach ( $goodss as $goods ) {
+            Goods::find($goods['id'])->update(['sequence' => $index]);
+            $index--;
+        }
+        return $this->apiReturn(true , '更新排序成功');
+    }
+}
